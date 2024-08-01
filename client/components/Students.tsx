a43f74bf0d345337db53016bd57dc9729a844d4a
Christy Liu <liusiyang915@gmail.com>
2024-08-01 13:51:05 +1200

    fixed bugs so database can show up

--- client/components/Students.tsx
+++ client/components/Students.tsx
@@ -1,16 +1,14 @@
-import { useFruits } from '../hooks/useFruits.ts'
-import { useStudents } from '../hooks/useStudents.ts'
+import useStudents from '../hooks/useStudents.ts'
 
 export default function Students() {
-  const { data } = useFruits()
-  //const { data } = useStudents()
+  const { data } = useStudents()
 
   return (
     <>
       <div className="app">
         <h1>List of current students:</h1>
         <ul>
-          {data && data.map((student) => <li key={student}>{student}</li>)}
+          {data && data.map((student: number) => <li key={student}>{student}</li>)}
         </ul>
       </div>
     </>
--- server/routes/courses.ts
+++ server/routes/courses.ts
@@ -6,7 +6,7 @@ const router = Router()
 router.get('/', async (req, res) => {
   try {
     const courses = await db.getAllCourses()
-    res.json({ courses: courses.map((course) => ({ name: course.name, description: course.description })) });
+    res.json(courses);
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: 'Something went wrong' })
